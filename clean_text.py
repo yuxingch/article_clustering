2134908937af9000f9fd518ae27d5a36562694c6
Yuxing Chen <yxchen2828@gmail.com>
2018-01-31 20:59:16 -0800

    update a few stop words

--- clean_text.py
+++ clean_text.py
@@ -14,14 +14,21 @@ def main():
         clean_row = re.sub('[^a-zA-Z]+', ' ', row['content'])
         contents.append(clean_row.lower())
     temp = ','.join(contents)
-        
-    sw.update(('www','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','https','http','dir','href','br','li')) #remove stop words
+    
+    #   add stop words to the set
+    sw.update(('image','com','href','utm','photo','inline','ltr','px','amp','figure','img',
+        'class', 'figcaption', 'blockquote', 'iframe','target','blank','src','www','a','b',
+        'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w',
+        'x','y','z','https','http','dir','href','br','li','style','font','width','margin',
+        'div','height','padding','hoodline','en','lang','subheading','frameborder','nl','es',
+        'uploads','ck','clktr','amazonaws','hoodwork','size','story','jpg')) 
+    
+    #   filtering out stop words in the corpus
     vocabulary = filter(lambda w: not w in sw, temp.split())
     output = ' '.join(vocabulary)
-    output_file = open('2_contents.txt', 'w')
+    output_file = open('0_contents.txt', 'w')
     output_file.write(output)
     output_file.close()
-
 
 if __name__ == "__main__":
     main()
